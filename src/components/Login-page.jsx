7ce09ec7c5699748bb0d97d8785f01a71d75882c
Mateen Mirani <mateenmirani125@gmail.com>
2022-11-12 12:11:07 +0500

    getting inputs values

--- src/components/Login-page.jsx
+++ src/components/Login-page.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logod.svg";
 
 const LogIn = () => {
+  const [enteredInp, setEnteredInp] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeHandler = (event) => {
+    setEnteredInp({ ...enteredInp, [event.target.name]: event.target.value });
+  };
+
+  const loginHandler = () => {
+    console.log(enteredInp);
+    setEnteredInp({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <>
       <div className="container flex flex-col mx-auto justify-center items-center mt-16 ">
@@ -18,16 +35,23 @@ const LogIn = () => {
             id="email"
             placeholder="Email address or phone number"
             className="px-4 h-14 my-2 rounded-md border border-1 border-gray-200 text-lg outline-none"
+            onChange={onChangeHandler}
+            value={enteredInp.email}
           />
           <input
-            type="pasword"
+            type="password"
             name="password"
             id="password"
             placeholder="Passsword"
             className="px-4 h-14 my-2 rounded-md border border-1 border-gray-200 text-lg outline-none"
+            onChange={onChangeHandler}
+            value={enteredInp.password}
           />
 
-          <button className="bg-blue-600 rounded-md py-3 text-white text-lg font-semibold hover:bg-blue-700 transition duration-300">
+          <button
+            onClick={loginHandler}
+            className="bg-blue-600 rounded-md py-3 text-white text-lg font-semibold hover:bg-blue-700 transition duration-300"
+          >
             Log In
           </button>
           <div className="flex justify-center my-4">
@@ -35,7 +59,6 @@ const LogIn = () => {
               Forgotten account?
             </span>
             <span className="text-blue-500 ml-1 hover:underline cursor-pointer">
-              {" "}
               Sign up for Facebook
             </span>
           </div>
